b829e63cc12966d7fe2e83f366f5ff39e3b7fa22
VladL14 <luncu.vlad@gmail.com>
2025-11-18 00:20:49 +0200

    a

--- backend/meditatii_server/settings.py
+++ backend/meditatii_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vbdmm1#umk-l*n$s#yu^4)heb*oqiij(e@peakdoaghm7!!n5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vladl14.pythonanywhere.com']
 
 
 # Application definition
@@ -125,7 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
